a1ec42be97236181c7b294713ae2b07e56c06969
Danny Hermes <daniel.j.hermes@gmail.com>
2018-10-18 11:40:33 -0700

    Stash

--- ndb/src/google/cloud/ndb/query.py
+++ ndb/src/google/cloud/ndb/query.py
@@ -15,6 +15,7 @@
 """High-level wrapper for datastore queries."""
 
 from google.cloud.ndb import _exceptions
+from google.cloud.ndb import model
 
 
 __all__ = [
@@ -174,8 +175,166 @@ OR = DisjunctionNode
 
 
 class Query:
-    def __init__(self, *args, **kwargs):
-        raise NotImplementedError
+    """Query object.
+
+    .. note::
+
+        A query is usually constructed by calling :meth:`Model.query`.
+
+    When any of the operators ``!=``, ``IN`` or ``OR`` is used, a
+    "multi-query" is produced. However, not  all operations on queries are
+    supported by "multi-query" instances.
+
+    Args:
+        kind (Optional[str]): The kind being queried.
+        ancestor (Optional[Key]): The ancestor key for the query.
+        filters (Optional[Node]): A single node representing a filter
+            expression tree.
+        orders (Optional[Any]): Low-level ``Order`` object.
+        app (Optional[str]): The Google Cloud Platform project (previously on
+            Google App Engine, this was called the Application ID).
+        namespace (Optional[str]): namespace.
+        default_options (Optional[QueryOptions]): The options for this query.
+        projection (Optional[Iterable[Property]]): Properties to project.
+        group_by (Optional[Iterable[Property]]): Properties to group by.
+    """
+
+    def __init__(
+        self,
+        *,
+        kind=None,
+        ancestor=None,
+        filters=None,
+        orders=None,
+        app=None,
+        namespace=None,
+        default_options=None,
+        projection=None,
+        group_by=None
+    ):
+        self._kind = kind
+        self._ancestor = self._validate_ancestor(ancestor, app, namespace)
+        self._filters = self._validate_filters(filters)
+        self._orders = self._validate_orders(orders)
+        self._app = app
+        self._namespace = self._validate_namespace(namespace, ancestor)
+        self._default_options = self._validate_default_options(default_options)
+        self._projection = self._validate_projection(projection, self._kind)
+        self._group_by = self._validate_group_by(group_by, self._kind)
+
+    @staticmethod
+    def _validate_ancestor(ancestor, app, namespace):
+        if ancestor is None:
+            return ancestor
+
+        if isinstance(ancestor, ParameterizedThing):
+            if (
+                isinstance(ancestor, ParameterizedFunction)
+                and ancestor.func != "key"
+            ):
+                raise TypeError(
+                    "ancestor cannot be a GQL function other than KEY"
+                )
+
+            return ancestor
+
+        if not isinstance(ancestor, model.Key):
+            raise TypeError(
+                "ancestor must be a Key; received {!r}".format(ancestor)
+            )
+
+        if not ancestor.id():
+            raise ValueError("ancestor cannot be an incomplete key")
+
+        if app is not None and app != ancestor.app():
+            raise TypeError("app/ancestor mismatch")
+
+        if namespace is not None and namespace != ancestor.namespace():
+            raise TypeError("namespace/ancestor mismatch")
+
+        return ancestor
+
+    @staticmethod
+    def _validate_filters(filters):
+        if filters is not None and not isinstance(filters, Node):
+            raise TypeError(
+                "filters must be a query Node or None; received {!r}".format(
+                    filters
+                )
+            )
+        return filters
+
+    @staticmethod
+    def _validate_orders(orders):
+        if orders is not None:
+            raise NotImplementedError("Missing datastore_query.Order")
+        return orders
+
+    @staticmethod
+    def _validate_default_options(default_options):
+        if default_options is not None:
+            raise NotImplementedError(
+                "Missing datastore_rpc.BaseConfiguration"
+            )
+        return default_options
+
+    @staticmethod
+    def _validate_namespace(namespace, ancestor):
+        if namespace is None and isinstance(ancestor, model.Key):
+            namespace = ancestor.namespace()
+        return namespace
+
+    @staticmethod
+    def _to_property_names(properties):
+        if not isinstance(properties, (list, tuple)):
+            properties = [properties]  # It will be type-checked below.
+        fixed = []
+        for prop in properties:
+            if isinstance(prop, str):
+                fixed.append(prop)
+            elif isinstance(prop, model.Property):
+                fixed.append(prop._name)
+            else:
+                raise TypeError(
+                    "Unexpected property ({!r}); should be string "
+                    "or Property".format(prop)
+                )
+
+        return fixed
+
+    @staticmethod
+    def _check_properties(fixed, kind, **kwargs):
+        modelclass = model.Model._kind_map.get(kind)
+        if modelclass is not None:
+            modelclass._check_properties(fixed, **kwargs)
+
+    def _validate_projection(self, projection, kind):
+        if projection is None:
+            return None
+
+        if not projection:
+            raise TypeError("projection argument cannot be empty")
+        if not isinstance(projection, (tuple, list)):
+            raise TypeError(
+                "projection must be a tuple, list or None; "
+                "received {!r}".format(projection)
+            )
+        self._check_properties(self._to_property_names(projection), kind)
+        return tuple(projection)
+
+    def _validate_group_by(self, group_by, kind):
+        if group_by is None:
+            return None
+
+        if not group_by:
+            raise TypeError("group_by argument cannot be empty")
+        if not isinstance(group_by, (tuple, list)):
+            raise TypeError(
+                "group_by must be a tuple, list or None; "
+                "received {!r}".format(group_by)
+            )
+        self._check_properties(self._to_property_names(group_by), kind)
+        return tuple(group_by)
 
 
 def gql(*args, **kwargs):
